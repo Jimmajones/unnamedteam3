aad801a33414f704b56cf187dcddab2e173f2c45
James Amanatidis <jamanatidis@student.unimelb.edu.au>
2022-08-25 19:22:56 +1000

    Added basic evolution and moves.

--- pokeset/pokedex/models.py
+++ pokeset/pokedex/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
-
+# All possible types among the Pokemon games.
+# TO-DO: Might want to store this in the database?
 TYPES = [
     ('NO', 'Normal'),
     ('FI', 'Fighting'),
@@ -26,6 +26,22 @@ TYPES = [
 
 class Pokemon(models.Model):
     name            = models.CharField(max_length=20)
-    description     = models.CharField(max_length=20, blank=True)
+    description     = models.CharField(max_length=200, blank=True)
+
+    # TO-DO: Want to ensure type_one and type_two are not the same.
     type_one        = models.CharField(max_length=2, choices=TYPES)
     type_two        = models.CharField(max_length=2, choices=TYPES, blank=True)
+
+    # TO-DO: Want to store information about evolution conditions. (Tertiary relationship?)
+    evolves_from    = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.name + ' (' + self.get_type_one_display() + ') (' + self.get_type_two_display() + ' )'
+
+class Move(models.Model):
+    name            = models.CharField(max_length=20)
+    type            = models.CharField(max_length=2, choices=TYPES)
+
+    def __str__(self):
+        return self.name + ' (' + self.get_type_display() + ')
+
