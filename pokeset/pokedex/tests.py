98a9d3a2b4c9d48c4af85d2349e30771aad482cc
tmblack01 <106416111+tmblack01@users.noreply.github.com>
2022-09-19 20:58:47 +1000

    Created test cases to check correct template was used for a request

--- pokeset/pokedex/tests.py
+++ pokeset/pokedex/tests.py
@@ -16,3 +16,20 @@ class AccessViewTestCase(TestCase):
     def test_login_without_login_path_access(self):
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
+
+class CorrectTemplateTestCase(TestCase):
+    """
+    Set of test cases that test if the right template was used
+    """
+
+    def test_login_access(self):
+        response = self.client.get('/login/')
+        self.assertTemplateUsed(response, 'login.html')
+
+    def test_register_access(self):
+        response = self.client.get('/register/')
+        self.assertTemplateUsed(response, 'register.html')
+
+    def test_login_without_login_path_access(self):
+        response = self.client.get('')
+        self.assertTemplateUsed(response, 'login.html')
