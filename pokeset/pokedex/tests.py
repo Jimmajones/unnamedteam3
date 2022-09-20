246ecaf052be70312c895be41058bd6ffc9e540c
tmblack01 <106416111+tmblack01@users.noreply.github.com>
2022-09-20 22:33:25 +1000

    added more test cases for login page

--- pokeset/pokedex/tests.py
+++ pokeset/pokedex/tests.py
@@ -25,6 +25,25 @@ class AccessViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
+class CorrectTemplateTestCase(TestCase):
+    """
+    Set of test cases that test if the right template was used
+    """
+
+    def test_login_template(self):
+        response = self.client.get('/login/')
+        self.assertTemplateUsed(response, 'login.html')
+
+    def test_register_template(self):
+        response = self.client.get('/register/')
+        self.assertTemplateUsed(response, 'register.html')
+
+    def test_login_without_login_path_template(self):
+        response = self.client.get('')
+        self.assertTemplateUsed(response, 'login.html')
+
+
+
 class AccessViewTestCaseWithSelenium(StaticLiveServerTestCase):
     """
     Set of test cases that test access to the webpages from other
@@ -49,22 +68,12 @@ class AccessViewTestCaseWithSelenium(StaticLiveServerTestCase):
         self.selenium.get(url + '/pokedex/login')
         self.selenium.find_element(By.CLASS_NAME, 'link_button').click()
         self.assertEqual(self.selenium.current_url, url + '/pokedex/register/')
+    
+    def test_click_login_button_with_no_login_details(self):
+        url = self.live_server_url
+        self.selenium.get(url + '/pokedex/login')
+        self.selenium.find_element(By.CLASS_NAME, "confirm_button").click()
+        self.assertEqual(self.selenium.current_url, url + '/pokedex/login/')
 
 
-class CorrectTemplateTestCase(TestCase):
-    """
-    Set of test cases that test if the right template was used
-    """
-
-    def test_login_template(self):
-        response = self.client.get('/login/')
-        self.assertTemplateUsed(response, 'login.html')
-
-    def test_register_template(self):
-        response = self.client.get('/register/')
-        self.assertTemplateUsed(response, 'register.html')
-
-    def test_login_without_login_path_template(self):
-        response = self.client.get('')
-        self.assertTemplateUsed(response, 'login.html')
 
