72a0e8df0289982a885c2d27822bf9b18da4b889
tmblack01 <106416111+tmblack01@users.noreply.github.com>
2022-09-25 11:28:11 +1000

    added test cases for login and registering account

--- pokeset/pokedex/tests.py
+++ pokeset/pokedex/tests.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
@@ -55,25 +54,100 @@ class AccessViewTestCaseWithSelenium(StaticLiveServerTestCase):
         super().setUpClass()
         chrome_options = Options()
         chrome_options.add_argument('--headless')
-        cls.selenium = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=chrome_options)
-        cls.selenium.implicitly_wait(10)
+        cls.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=chrome_options)
+        cls.driver.set_window_size(1024, 768)
+        cls.driver.implicitly_wait(5)
     
     @classmethod
     def tearDownClass(cls):
-        cls.selenium.quit()
+        cls.driver.quit()
         return super().tearDownClass()
 
     def test_register_access_from_login_page(self):
         url = self.live_server_url
-        self.selenium.get(url + '/pokedex/login')
-        self.selenium.find_element(By.CLASS_NAME, 'link_button').click()
-        self.assertEqual(self.selenium.current_url, url + '/pokedex/register/')
+        self.driver.get(url + '/pokedex/login')
+        self.driver.find_element(By.CLASS_NAME, 'link_button').click()
+        self.assertEqual(self.driver.current_url, url + '/pokedex/register/')
+
+    def test_register_access_from_login_page(self):
+        url = self.live_server_url
+        self.driver.get(url + '/pokedex/register')
+        self.driver.find_element(By.CLASS_NAME, 'link_back_button').click()
+        self.assertEqual(self.driver.current_url, url + '/pokedex/login/')
     
+
+class RegisterAndLoginTestCase(StaticLiveServerTestCase):
+    """
+    Set of test cases that test access to test successfully registering
+    a new user and login
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        cls.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=chrome_options)
+        cls.driver.set_window_size(1024, 768)
+        cls.driver.implicitly_wait(5)
+    
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        return super().tearDownClass()
+
+    def test_registering_new_account(self):
+        url = self.live_server_url
+        self.driver.get(url + '/pokedex/login')
+        self.driver.find_element(By.CLASS_NAME, 'link_button').click()
+        self.driver.find_element(By.NAME, 'username').send_keys('test_user')
+        self.driver.find_element(By.NAME, 'email').send_keys('test@example.com')
+        self.driver.find_element(By.NAME, 'password1').send_keys('secret#1')
+        self.driver.find_element(By.NAME, 'password2').send_keys('secret#1')
+        self.driver.find_element(By.CLASS_NAME, 'confirm_button').click()
+        self.assertEqual(self.driver.current_url, url + '/pokedex/login/')
+
+    def test_login_new_account(self):
+        url = self.live_server_url
+        self.driver.get(url + '/pokedex/login')
+        self.driver.find_element(By.CLASS_NAME, 'link_button').click()
+        self.driver.find_element(By.NAME, 'username').send_keys('test_user')
+        self.driver.find_element(By.NAME, 'email').send_keys('test@example.com')
+        self.driver.find_element(By.NAME, 'password1').send_keys('secret#1')
+        self.driver.find_element(By.NAME, 'password2').send_keys('secret#1')
+        self.driver.find_element(By.CLASS_NAME, 'confirm_button').click()
+        self.driver.find_element(By.NAME, 'username').send_keys('test_user')
+        self.driver.find_element(By.NAME, 'password').send_keys('secret#1')
+        self.driver.find_element(By.CLASS_NAME, 'confirm_button').click()
+        self.assertEqual(self.driver.current_url, url + '/pokedex/profiles/')
+    
+    def test_user_can_successfully_logout(self):
+        url = self.live_server_url
+        self.driver.get(url + '/pokedex/login')
+        self.driver.find_element(By.CLASS_NAME, 'link_button').click()
+        self.driver.find_element(By.NAME, 'username').send_keys('test_user')
+        self.driver.find_element(By.NAME, 'email').send_keys('test@example.com')
+        self.driver.find_element(By.NAME, 'password1').send_keys('secret#1')
+        self.driver.find_element(By.NAME, 'password2').send_keys('secret#1')
+        self.driver.find_element(By.CLASS_NAME, 'confirm_button').click()
+        self.driver.find_element(By.NAME, 'username').send_keys('test_user')
+        self.driver.find_element(By.NAME, 'password').send_keys('secret#1')
+        self.driver.find_element(By.CLASS_NAME, 'confirm_button').click()
+        self.driver.find_element(By.CLASS_NAME, 'back_button').click()
+        self.assertEqual(self.driver.current_url, url + '/pokedex/login/') 
+
     def test_click_login_button_with_no_login_details(self):
         url = self.live_server_url
-        self.selenium.get(url + '/pokedex/login')
-        self.selenium.find_element(By.CLASS_NAME, "confirm_button").click()
-        self.assertEqual(self.selenium.current_url, url + '/pokedex/login/')
+        self.driver.get(url + '/pokedex/login')
+        self.driver.find_element(By.CLASS_NAME, "confirm_button").click()
+        self.assertEqual(self.driver.current_url, url + '/pokedex/login/') 
+
+    def test_click_register_button_with_no_register_details(self):
+        url = self.live_server_url
+        self.driver.get(url + '/pokedex/register')
+        self.driver.find_element(By.CLASS_NAME, "confirm_button").click()
+        self.assertEqual(self.driver.current_url, url + '/pokedex/register/')    
+
 
 
 
