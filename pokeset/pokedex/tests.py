c7f25df361d21c8ddfc890303caf38215395b7ef
tmblack01 <106416111+tmblack01@users.noreply.github.com>
2022-10-03 22:36:56 +1100

    Included firefox browser in one of the test cases

--- pokeset/pokedex/tests.py
+++ pokeset/pokedex/tests.py
@@ -2,9 +2,14 @@ from django.test import TestCase
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FireFoxService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options as FireFoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
+from django.contrib.auth.models import User
+from django.contrib.auth.models import UserManager
 
 # URLs for testing
 LOGIN_URL = '/accounts/login/'
@@ -64,84 +69,96 @@ class AccessViewTestCaseWithSelenium(StaticLiveServerTestCase):
     def setUpClass(cls):
         # set up webdriver for test cases
         super().setUpClass()
-        chrome_options = Options()
+        chrome_options = ChromeOptions()
         chrome_options.add_argument('--headless')
-        cls.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=chrome_options)
-        cls.driver.set_window_size(1024, 768)
-        cls.driver.implicitly_wait(5)
+        cls.chrome_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=chrome_options)
+        cls.chrome_driver.set_window_size(1024, 768)
+        cls.chrome_driver.implicitly_wait(5)
+        firefox_options = FireFoxOptions()
+        firefox_options.add_argument('--headless')
+        cls.firefox_driver = webdriver.Firefox(service=FireFoxService(GeckoDriverManager().install()),options=firefox_options)
+        cls.firefox_driver.set_window_size(1024, 768)
+        cls.firefox_driver.implicitly_wait(5)
     
     @classmethod
     def tearDownClass(cls):
-        # quit webdriver after all test cases have run
-        cls.driver.quit()
+        # quit driver after all test cases have run
+        cls.chrome_driver.quit()
+        cls.firefox_driver.quit()
         return super().tearDownClass()
 
     def test_login_access_from_register_page(self):
         # go to register webpage
         url = self.live_server_url
-        self.driver.get(url + REGISTER_URL)
+        self.chrome_driver.get(url + REGISTER_URL)
 
         # click back button and check if user is in the login webpage
-        self.driver.find_element(By.CLASS_NAME, BACK_CLASS).click()
-        self.assertEqual(self.driver.current_url, url + LOGIN_URL) 
+        self.chrome_driver.find_element(By.CLASS_NAME, BACK_CLASS).click()
+        self.assertEqual(self.chrome_driver.current_url, url + LOGIN_URL)
+
+        self.firefox_driver.get(url + REGISTER_URL)
+
+        # click back button and check if user is in the login webpage
+        self.firefox_driver.find_element(By.CLASS_NAME, BACK_CLASS).click()
+        self.assertEqual(self.firefox_driver.current_url, url + LOGIN_URL) 
 
     def test_text_can_be_enter_in_username_input_login_page(self):
         # go to login webpage
         url = self.live_server_url
-        self.driver.get(url + LOGIN_URL)
+        self.chrome_driver.get(url + LOGIN_URL)
 
         # get username input and check if text can be entered in the input box
-        username_input = self.driver.find_element(By.NAME, 'username')
+        username_input = self.chrome_driver.find_element(By.NAME, 'username')
         username_input.send_keys('username')
         self.assertEqual(username_input.get_attribute('value'), 'username') 
 
     def test_text_can_be_enter_in_password_input_login_page(self):
         # go to login webpage
         url = self.live_server_url
-        self.driver.get(url + LOGIN_URL)
+        self.chrome_driver.get(url + LOGIN_URL)
 
         # get password input and check if text can be entered in the input box
-        username_input = self.driver.find_element(By.NAME, 'password')
+        username_input = self.chrome_driver.find_element(By.NAME, 'password')
         username_input.send_keys('secret#1')
         self.assertEqual(username_input.get_attribute('value'), 'secret#1')
     
     def test_text_can_be_enter_in_username_input_register_page(self):
         # go to register webpage
         url = self.live_server_url
-        self.driver.get(url + REGISTER_URL)
+        self.chrome_driver.get(url + REGISTER_URL)
 
         # get username input and check if text can be entered in the input box
-        username_input = self.driver.find_element(By.NAME, 'username')
+        username_input = self.chrome_driver.find_element(By.NAME, 'username')
         username_input.send_keys('username')
         self.assertEqual(username_input.get_attribute('value'), 'username')
     
     def test_text_can_be_enter_in_email_input_register_page(self):
         # go to register webpage
         url = self.live_server_url
-        self.driver.get(url + REGISTER_URL)
+        self.chrome_driver.get(url + REGISTER_URL)
 
         # get email input and check if text can be entered in the input box
-        username_input = self.driver.find_element(By.NAME, 'email')
+        username_input = self.chrome_driver.find_element(By.NAME, 'email')
         username_input.send_keys('test@example.com')
         self.assertEqual(username_input.get_attribute('value'), 'test@example.com')
     
     def test_text_can_be_enter_in_password1_input_register_page(self):
         # go to register webpage
         url = self.live_server_url
-        self.driver.get(url + REGISTER_URL)
+        self.chrome_driver.get(url + REGISTER_URL)
 
         # get password1 input and check if text can be entered in the input box
-        username_input = self.driver.find_element(By.NAME, 'password1')
+        username_input = self.chrome_driver.find_element(By.NAME, 'password1')
         username_input.send_keys('secret')
         self.assertEqual(username_input.get_attribute('value'), 'secret')
     
     def test_text_can_be_enter_in_password2_input_register_page(self):
         # go to register webpage
         url = self.live_server_url
-        self.driver.get(url + REGISTER_URL)
+        self.chrome_driver.get(url + REGISTER_URL)
 
         # get password2 input and check if text can be entered in the input box
-        username_input = self.driver.find_element(By.NAME, 'password2')
+        username_input = self.chrome_driver.find_element(By.NAME, 'password2')
         username_input.send_keys('secret')
         self.assertEqual(username_input.get_attribute('value'), 'secret')
 
@@ -155,7 +172,7 @@ class RegisterAndLoginTestCase(StaticLiveServerTestCase):
     def setUpClass(cls):
         # set up webdriver for test cases
         super().setUpClass()
-        chrome_options = Options()
+        chrome_options = ChromeOptions()
         chrome_options.add_argument('--headless')
         cls.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=chrome_options)
         cls.driver.set_window_size(1024, 768)
@@ -180,12 +197,12 @@ class RegisterAndLoginTestCase(StaticLiveServerTestCase):
     def test_login_new_account(self):
         # go to register webpage
         url = self.live_server_url
-        self.driver.get(url + REGISTER_URL)
+        self.driver.get(url + LOGIN_URL)
 
         # register new account and login and check if user 
         # has successfully signed into account
-        register_account(self.driver, 'test_user', 'test@example.com', 'secret#1', 'secret#1')
-        login_to_account(self.driver, 'test_user', 'secret#1')
+        #register_account(self.driver, 'test_user', 'test@example.com', 'secret#1', 'secret#1')
+        login_to_account(self.driver, 'another_user', 'secretpassword1')
         self.assertEqual(self.driver.current_url, url + PROFILES_URL)
     
     def test_user_can_successfully_logout(self):
